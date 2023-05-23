ea1233bb494c49a8999053bd56a29b3a1b82a71d
Nitzan Raz <nitz.raz@gmail.com>
2023-05-23 11:57:04 +0300

    new CV and fixed fetcher

--- fetch-cv.sh
+++ fetch-cv.sh
@@ -2,6 +2,6 @@
 set -e
 DOCX_FILE=$(mktemp)
 OUT_DIR=$(mktemp -d)
-rclone cat 'drive:new job/Resume.docx' >$DOCX_FILE
+rclone cat 'drive:work/new job/Resume.docx' >$DOCX_FILE
 lowriter --convert-to pdf $DOCX_FILE --outdir $OUT_DIR
 mv $OUT_DIR/*.pdf "$1"
